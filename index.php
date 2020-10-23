81875079babfb1e0a32040bfa914c8c0296b6d0d
FreekAlberti <federicoalberi96@gmail.com>
2020-10-23 16:47:45 +0200

    tabella 2 istanze

--- index.php
+++ index.php
@@ -4,6 +4,13 @@ class Uomo {
     public $nome;
     public $cognome;
     public $anno;
+
+    function __construct($nome, $cognome, $anno)
+        {
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->anno = $anno;
+        }
     }
     
 class Professione extends Uomo {
@@ -12,15 +19,15 @@ class Professione extends Uomo {
     public $retribuzione;
 }
 
-$freek = new Professione();
-$freek->nome = "Freek";
-$freek->cognome = "Albertie";
-$freek->anno = 1996;
-$freek->ruolo = "Web Developer";
+$freek = new Professione("Freek", "Albertie", 1996);
+$freek->ruolo = "Frontend Developer";
 $freek->azienda = "IBM";
 $freek->retribuzione = "1000€";
 
-var_dump($freek);die;
+$sido = new Professione("Sido", "Don", 1996);
+$sido->ruolo = "Backend Developer";
+$sido->azienda = "Canonical";
+$sido->retribuzione = "1100€";
 
 ?>
 
@@ -32,6 +39,35 @@ var_dump($freek);die;
     <title>Document</title>
 </head>
 <body>
-    <h1><?php  ?></h1>    
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Cognome</th>
+                <th>Anno</th>
+                <th>Ruolo</th>
+                <th>Azienda</th>
+                <th>Retribuzione</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $freek->nome ?></td>
+                <td><?php echo $freek->cognome ?></td>
+                <td><?php echo $freek->anno ?></td>
+                <td><?php echo $freek->ruolo ?></td>
+                <td><?php echo $freek->azienda ?></td>
+                <td><?php echo $freek->retribuzione ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $sido->nome ?></td>
+                <td><?php echo $sido->cognome ?></td>
+                <td><?php echo $sido->anno ?></td>
+                <td><?php echo $sido->ruolo ?></td>
+                <td><?php echo $sido->azienda ?></td>
+                <td><?php echo $sido->retribuzione ?></td>
+            </tr>
+        </tbody>
+    </table> 
 </body>
 </html>
